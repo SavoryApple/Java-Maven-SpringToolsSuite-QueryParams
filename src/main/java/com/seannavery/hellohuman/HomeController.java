2c4cbe0f34b0669c1fbd469bca42a65ae2bb292a
Seann Avery <SeannAvery@gmail.com>
2022-04-08 13:14:35 -0700

    added example for PATH variables, instead of querySearch

--- src/main/java/com/seannavery/hellohuman/HomeController.java
+++ src/main/java/com/seannavery/hellohuman/HomeController.java
@@ -1,5 +1,6 @@
 package com.seannavery.hellohuman;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,15 +9,29 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 	
 	
-    @RequestMapping("/")
-    public String index(
-    		@RequestParam(value="name", required=false) String firstName,
-    		@RequestParam(value="last_name", required=false) String lastName) {
-    	if(firstName != null && lastName != null) {	
-    		return "You searched for: " + firstName + " " + lastName;
+//    @RequestMapping("/")
+//    public String index(
+//    		@RequestParam(value="name", required=false) String firstName,
+//    		@RequestParam(value="last_name", required=false) String lastName) {
+//    	if(firstName != null && lastName != null) {	
+//    		return "You searched for: " + firstName + " " + lastName;
+//    	}
+//    	else {
+//    		return "Hello human!";
+//    	}
+//    }
+	@RequestMapping("/daikichi/travel/{city}")
+    public String index(@PathVariable("city") String city) {
+    		return "congratulations, you will soon travel to " + city + "!";
+    }
+    
+    @RequestMapping("/daikichi/lotto/{integer}")
+    public String index(@PathVariable("integer") Integer integer) {
+    	if(integer %2 == 0) {
+    		return "You will take a grand journey in the near future, but be weary of tempting offers";
     	}
     	else {
-    		return "Hello human!";
+    		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
     	}
     }
 }
